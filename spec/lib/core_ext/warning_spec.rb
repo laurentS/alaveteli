d67fa11aadc17829a6d1997f193bdc820c62f889
Gareth Rees <gareth@garethrees.co.uk>
2020-12-10 12:35:33 +0000

    fixup! Prevent CensorRules being saved when Regexp is bad

--- spec/lib/core_ext/warning_spec.rb
+++ spec/lib/core_ext/warning_spec.rb
@@ -1,15 +1,16 @@
 require 'spec_helper'
 
-describe Warning do
+klass = RUBY_VERSION < '2.4.0' ? Kernel : Warning
+describe klass do
   describe '.with_raised_warnings' do
     it 'makes .warn raise' do
       expect {
-        described_class.with_raised_warnings { Warning.warn('foo') }
+        described_class.with_raised_warnings { described_class.warn('foo') }
       }.to raise_error(RaisedWarning)
     end
 
     it 'only affects calls to .warn within the block' do
-      expect { Warning.warn('bar') }.to output('bar').to_stderr
+      expect { described_class.warn('bar') }.to output('bar').to_stderr
     end
   end
 end
